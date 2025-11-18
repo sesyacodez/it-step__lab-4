f05801dc625ae4e4eba8b207b589f6449b08784f
Stefania <62096135+sesyacodez@users.noreply.github.com>
2025-11-18 22:01:09 +0000

    tweaks

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".header__burger");
-const menuContainer = document.querySelector(".header__menu-container");
-
-burger.addEventListener("click", () => {
-  menuContainer.classList.toggle("active");
-});
